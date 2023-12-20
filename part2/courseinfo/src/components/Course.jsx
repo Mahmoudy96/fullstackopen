83948006b2f79f4991dc3c6915e0171e19e84377
Mahmoudy96 <mahmoudyaz96@gmail.com>
2023-12-20 11:49:43 +0200

    finished part 2 course info

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((cur,part) => part.exercises+cur,0)
+  return(
+  <p>Number of exercises {sum}</p>
+  )
+}
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+  console.log(parts)
+  return(<>
+    {parts.map(part => <Part key={part.id} part={part}></Part>)}
+  </>)
+}
+const Course = ({course}) => {
+  const {id, name, parts} = course
+  return(    <div>
+    <Header course={name} />
+    <Content
+      parts = {parts}
+    />
+    <Total parts={parts} />
+  </div>
+  )
+}
+
+export default Course
